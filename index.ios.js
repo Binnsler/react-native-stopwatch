88d99eef5246a529b760f43ba6e3007e52180a7e
marczenn <jamie.m.binns@gmail.com>
2016-08-18 17:34:12 -0600

    laps not working correctly

---
+++ index.ios.js
@@ -0,0 +1,188 @@
+import React from 'react';
+import {View, Text, AppRegistry, StyleSheet, TouchableHighlight} from 'react-native';
+import MinSecMilli from 'minutes-seconds-milliseconds';
+
+
+var StopWatch = React.createClass({
+
+  getInitialState: function(){
+    return {
+      timeElapsed: null,
+      isRunning: false,
+      startTime: null,
+      isPaused: false,
+      pauseStart: null,
+      totalPause: 0,
+      lastLapTime: 0,
+      laps: []
+    }
+  },
+
+  render: function(){
+    return (
+      <View style={styles.container}>
+        <View style={[styles.header]}>
+          <View style={[styles.timerWrapper]}>
+            <Text style={styles.timer}>{MinSecMilli(this.state.timeElapsed)}</Text>
+          </View>
+          <View style={[styles.buttonWrapper]}>
+            {this.startPauseButton()}
+            {this.stopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+        <View style={[styles.footer]}>
+          {this.laps()}
+        </View>
+      </View>
+    );
+  },
+
+  startPauseButton: function(){
+    return (
+      <TouchableHighlight style={[styles.button, this.startPauseStyle()]} underlayColor="gray" onPress={this.handleStartPress}><Text>{this.state.isRunning ? 'Pause' : 'Start'}</Text></TouchableHighlight>
+    );
+  },
+
+  stopButton: function(){
+    return (
+      <TouchableHighlight style={[styles.button, {borderColor: 'red'}]} onPress={this.handleStopPress}><Text>Stop</Text></TouchableHighlight>
+    );
+  },
+
+  handleStopPress: function(){
+    console.log('stop')
+  },
+
+  startPauseStyle: function(){
+    return this.state.isRunning ? {borderColor: 'blue'} : {borderColor: 'green'}
+  },
+
+  lapButton: function(){
+    return (
+      <TouchableHighlight style={styles.button} underlayColor="gray" onPress={this.handleLapPress}><Text>Lap</Text></TouchableHighlight>
+    );
+  },
+
+  handleStartPress: function(){
+    // Pause
+    if(this.state.isRunning){
+      clearInterval(this.interval);
+      this.setState({
+        isRunning: false,
+        pauseStart: new Date(),
+        isPaused: true,
+        timeElapsed: new Date() - this.state.totalPause - this.state.startTime
+      });
+      return;
+    }
+
+    // Unpause
+    if(this.state.isPaused){
+
+      var pausedTime = new Date() - this.state.pauseStart;
+      pausedTime += this.state.totalPause;
+
+      this.setState({totalPause: pausedTime})
+
+      this.interval = setInterval(function(){
+        this.setState({
+          isRunning: true,
+          isPaused: false,
+          timeElapsed: new Date() - this.state.totalPause - this.state.startTime
+        });
+      }.bind(this), 30)
+    }
+
+    else {
+      this.setState({
+        startTime: new Date()
+      })
+
+      this.interval = setInterval(function(){
+        this.setState({
+          timeElapsed: new Date() - this.state.startTime,
+          isRunning: true
+        });
+      }.bind(this), 30);
+    }
+  },
+
+  handleLapPress: function(){
+
+    if(this.state.isRunning){
+      var lapTime = this.state.timeElapsed - this.state.lastLapTime;
+
+      this.setState({
+        lastLapTime: lapTime,
+        laps: this.state.laps.concat([lapTime])
+      });
+    }
+
+    return;
+  },
+
+  laps: function(){
+    return this.state.laps.map(function(time, i){
+      return (
+        <View key={i} style={styles.lap}>
+        <Text style={styles.lapText}>Lap #{i + 1}: </Text>
+        <Text style={styles.lapText}>{MinSecMilli(time)}</Text>
+        </View>
+      )
+    })
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+
+  header: {
+    flex: 1
+  },
+
+  footer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  timerWrapper: {
+    flex: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  buttonWrapper: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+
+  timer: {
+    fontSize: 60
+  },
+
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  lap: {
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+
+  lapText: {
+    fontSize: 30
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', function(){return StopWatch});
